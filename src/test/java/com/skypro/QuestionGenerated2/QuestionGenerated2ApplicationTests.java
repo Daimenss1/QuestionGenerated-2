78f572981c84dd6b6c952565f6207ba2ab87df79
dgobozov <saniamit@mail.ru>
2022-05-26 18:08:10 +0300

    КС № 2

---
+++ src/test/java/com/skypro/QuestionGenerated2/QuestionGenerated2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.QuestionGenerated2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionGenerated2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
